18e09293efb087776217421784b61a5bec2a5fa8
JFreud <jrm.frdnbrg@gmail.com>
2017-11-09 21:53:02 -0500

    started

--- restapi.py
+++ restapi.py
@@ -3,7 +3,9 @@ import urllib2, json
 
 api_app = Flask (__name__)
 
-
+u = urllib2.urlopen("http://api.nasa.gov/planetary/apod?api_key=S93VcKmv5nCKEmbxi3459T5wo5UkuOrwDdT3vghB")
+not_dict = u.read()
+real_dict = json.loads(not_dict)
 
 #getting error urllib2.URLError: <urlopen error EOF occurred in violation of protocol (_ssl.c:590)>
 
@@ -12,9 +14,7 @@ api_app = Flask (__name__)
 
 @api_app.route("/")
 def root():
-    u = urllib2.urlopen("http://api.nasa.gov/planetary/apod?api_key=S93VcKmv5nCKEmbxi3459T5wo5UkuOrwDdT3vghB")
-    not_dict = u.read()
-    real_dict = json.loads(not_dict)
+
     return render_template("root.html", url = real_dict["hdurl"], text = real_dict["explanation"])
 
 
